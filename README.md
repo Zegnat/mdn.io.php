07dc179250619c596dde8b9bd5d6b4d569b9719e
Martijn van der Ven <martijn@zegnat.net>
2013-09-17 13:15:43 +0200

    Rough draft.

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 mdn.io.php
 ==========
 
-A port of @lazd’s mdn.io to PHP. An “I’m feeling lucky” URL shortener.
+A port of [Davis]’ ([@lazd]) [mdn.io] to PHP. An “I’m feeling lucky” URL shortener.
+
+Currently `server.php` is made to run as a _Router Script_ for PHP’s built-in web server. Execute with the following command:
+
+```
+php -S localhost:1337 server.php
+```
+
+For available settings, please see [the original README](https://github.com/lazd/mdn.io/blob/master/README.md). Environment variables can be set as follows:
+
+```
+SERVICE=ddg php -d variables_order=ES -S localhost:1337 server.php
+```
+
+
+[Davis]: http://lazd.net/
+[@lazd]: https://github.com/lazd
+[mdn.io]: https://github.com/lazd/mdn.io/
---
+++ server.php
@@ -0,0 +1,38 @@
+<?php
+
+	$serviceURLs = array(
+		'google' => 'http://google.com/search?btnI&q=',
+		'bing' => 'http://www.bing.com/search?q=',
+		'ddg' => 'https://duckduckgo.com/?q=%21+'
+	);
+
+	$defaults = array(
+		'SERVICE' => 'google',
+		'SEARCH_DOMAIN' => 'developer.mozilla.org',
+		'FALLBACK_URL' => 'https://developer.mozilla.org/en-US/docs/JavaScript'
+	)
+
+	foreach ($defaults as $var => $default) {
+		if (isset($_ENV[$var])) {
+			$$var = $_ENV[$var];
+		} else {
+			$$var = $default;
+		}
+	}
+
+	$query = substr(urldecode($_SERVER['REQUEST_URI']),1);
+	$url = empty($query) ? $FALLBACK_URL
+	                     : (isset($serviceURLs[$SERVICE]) ? $serviceURLs[$SERVICE]
+	                                                      : $serviceURLs[$defaults['SERVICE']])
+	                     . urlencode($query.' site:'.$SEARCH_DOMAIN);
+
+	file_put_contents("php://stdout", sprintf(
+		"%s => %s\n",
+		empty($query) ? '(empty query)' : $query,
+		$url
+	));
+
+	header('Location: '.$url, true, 303);
+	exit();
+
+?>
